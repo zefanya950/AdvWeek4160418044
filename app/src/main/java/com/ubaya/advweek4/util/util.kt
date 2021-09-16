694d9a146a7664dca066550cec306889242b044e
zefanya950 <zardyaprayuska89@gmail.com>
2021-09-16 12:22:28 +0700

    Android KTX Done, Belum HomeWork

---
+++ app/src/main/java/com/ubaya/advweek4/util/util.kt
@@ -0,0 +1,27 @@
+package com.ubaya.advweek4.util
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import com.ubaya.advweek4.R
+import java.lang.Exception
+
+fun ImageView.loadImage(url:String,progressBar:ProgressBar){
+    Picasso.get()
+        .load(url)
+        .resize(400,400)
+        .centerCrop()
+        .error(R.drawable.ic_baseline_error_24)
+        .into(this, object:Callback{
+            override fun onSuccess() {
+                progressBar.visibility = View.GONE
+            }
+
+            override fun onError(e: Exception?) {
+
+            }
+
+        })
+}
--- app/src/main/java/com/ubaya/advweek4/view/StudentListAdapter.kt
+++ app/src/main/java/com/ubaya/advweek4/view/StudentListAdapter.kt
@@ -7,6 +7,7 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ubaya.advweek4.R
 import com.ubaya.advweek4.model.Student
+import com.ubaya.advweek4.util.loadImage
 import kotlinx.android.synthetic.main.student_list_item.view.*
 
 class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
@@ -27,6 +28,7 @@ class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapte
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
         holder.view.txtID.text = studentList[position].id
         holder.view.txtName.text = studentList[position].name
+        holder.view.imageView.loadImage(studentList[position].photoUrl.toString(),holder.view.progressBar)
 
         holder.view.btnDetail.setOnClickListener {
             val action = StudentListFragmentDirections.actionStudentDetail()
